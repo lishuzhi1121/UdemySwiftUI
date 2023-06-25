c11ee13a64e95d12b6cde03dacf77a5b863a5bff
Sands <shuzhi.li@quvideo.com>
2023-06-25 10:44:16 +0800

    SwiftUI AsyncImage

---
+++ SwiftUI-AsyncImage/SwiftUI-AsyncImage/SwiftUI_AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AsyncImageApp.swift
+//  SwiftUI-AsyncImage
+//
+//  Created by Sands on 2023/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
